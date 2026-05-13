8f9be0f3c7e222d585baec290958780d8a65b6a6
AlvaroPiedra <uo281879@uniovi.es>
2026-05-13 22:58:50 +0200

    chore: bootstrap backend project

---
+++ backend/src/main/java/com/econocom/login/BackendApplication.java
@@ -0,0 +1,20 @@
+package com.econocom.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Spring Boot application class.
+ */
+@SpringBootApplication
+public class BackendApplication {
+
+    /**
+     * Application entry point.
+     *
+     * @param args program arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
